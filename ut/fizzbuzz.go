f09ed7c530a5910f7e41c7035e65e0d2da78bf5f
iszkrit <iszkrit@g.ecc.u-tokyo.ac.jp>
2023-05-19 17:35:19 +0900

    commit10

--- ut/fizzbuzz.go
+++ ut/fizzbuzz.go
@@ -28,6 +28,6 @@ func FizzBuzz(numbers []int) []string {
 }
 
 func main() {
-	var test = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	var test = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
 	fmt.Printf("[]string : %v\n", FizzBuzz(test))
 }
